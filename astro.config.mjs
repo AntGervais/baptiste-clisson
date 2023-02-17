a7f8a4d1d81fbda1a9837169e3818e9312c5f90f
Antoine Gervais <98803358+AntoineGerv@users.noreply.github.com>
2023-02-17 15:06:41 -0400

    feat: add admin

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,6 @@ import sitemap from '@astrojs/sitemap';
 import image from '@astrojs/image';
 import partytown from '@astrojs/partytown';
 import compress from 'astro-compress';
-import NetlifyCMS from 'astro-netlify-cms';
 import react from '@astrojs/react';
 
 import { SITE } from './src/config.mjs';
@@ -59,100 +58,6 @@ export default defineConfig({
     }),
 
     react(),
-    NetlifyCMS({
-      config: {
-        // Use Netlify’s “Git Gateway” authentication and target our default branch
-        backend: {
-          name: 'git-gateway',
-          branch: 'main',
-        },
-        base_url: 'https://charpenteboisdebout.fr/',
-        // Configure where our media assets are stored & served from
-        media_folder: '/public/images',
-        public_folder: '/public/images',
-        // Configure the content collections
-        collections: [
-          {
-            name: 'imageAccueil',
-            label: 'Images accueil',
-            label_singular: 'image',
-            folder: 'src/content/imageAccueil',
-            create: true,
-            delete: true,
-            fields: [
-              {
-                name: 'image',
-                widget: 'image',
-                label: 'Image principale',
-                required: true,
-              },
-              {
-                name: 'description',
-                widget: 'string',
-                label: 'Description',
-                required: true,
-              },
-            ],
-          },
-          {
-            name: 'posts',
-            label: 'Posts',
-            label_singular: 'Post',
-            folder: 'src/content/post',
-            create: true,
-            delete: true,
-            fields: [
-              {
-                name: 'title',
-                widget: 'string',
-                label: 'Titre',
-                required: true,
-              },
-              {
-                name: 'image',
-                widget: 'image',
-                label: 'Image principale',
-                required: true,
-              },
-              {
-                name: 'description',
-                widget: 'string',
-                label: 'Description',
-                required: true,
-              },
-              {
-                name: 'body',
-                widget: 'markdown',
-                label: 'body',
-              },
-              {
-                name: 'tags',
-                widget: 'select',
-                label: 'Tags',
-                multiple: true,
-                min: 1,
-                options: ['charpente', 'nouveau', 'rénovation'],
-              },
-              {
-                name: 'publishDate',
-                widget: 'datetime',
-                format: 'DD MMM YYYY',
-                date_format: 'DD MMM YYYY',
-                time_format: false,
-                label: 'Publish Date',
-              },
-              {
-                name: 'accueil',
-                widget: 'boolean',
-                label: 'Afficher page accueil',
-                required: false,
-              },
-            ],
-          },
-        ],
-      },
-      previewStyles: ['https://fonts.googleapis.com/css2?family=Roboto&display=swap', '/src/styles/cms.css'],
-    }),
   ],
 
   vite: {
