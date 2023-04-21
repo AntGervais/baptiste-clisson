9be849bc2119bf168ddf4803835ffcb3c67dcc0d
Antoine Gervais <98803358+AntoineGerv@users.noreply.github.com>
2023-04-21 15:16:08 -0400

    fix: form contatc

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -37,81 +37,83 @@ export default function ContactForm() {
   })
 
   return (
-    <div className="my-2 lg:my-0 bg-gray-100 rounded-sm">
-      <div className="form-container relative w-full rounded-sm shadow-lg bg-blackAlpha-600">
-        <div
-          className={[
-            'form-overlay',
-            'absolute',
-            'h-full',
-            'inset-0',
-            'rounded-sm',
-            'z-20',
-            'bg-gray-900',
-            'opacity-70',
-            submitting ? 'centered' : 'hidden',
-          ].join(' ')}
-        >
-          <PulseSpinner />
+    <div className='lg:absolute lg:right-12 w-full'>
+      <div className="bg-gray-100 rounded-sm">
+        <div className="form-container relative w-full rounded-sm shadow-lg bg-blackAlpha-600">
+          <div
+            className={[
+              'form-overlay',
+              'absolute',
+              'h-full',
+              'inset-0',
+              'rounded-sm',
+              'z-20',
+              'bg-gray-900',
+              'opacity-70',
+              submitting ? 'centered' : 'hidden',
+            ].join(' ')}
+          >
+            <PulseSpinner />
+          </div>
+
+          <form
+            onSubmit={handleSubmit}
+            noValidate
+            className="relative w-full p-4 sm:py-6 md:py-8 md:px-10 rounded-sm"
+          >
+            {serverError && (
+              <div className="mb-4 sm:mb-6 bg-red-500 py-4 rounded-sm text-center">
+                <h3 className="text2xl text-white">{serverError}</h3>
+              </div>
+            )}
+            {submitted && (
+              <div className="mb-4 sm:mb-6 bg-green-500 py-4 rounded-sm text-center">
+                <h3 className="text2xl text-white">Message successfully sent!</h3>
+              </div>
+            )}
+            <div className="flex flex-col gap-2">
+
+              <Input
+                name="name"
+                label="Nom"
+                required
+                bg="bg-gray-100"
+                {...register('name')}
+              />
+              <Input
+                name="email"
+                label="Email"
+                required
+                bg="bg-gray-100"
+                {...register('email')}
+              />
+              <Input name="phone" label="Numéro de téléphone" bg="bg-gray-100" {...register('phone')} />
+              <TextArea
+                name="message"
+                label="Message"
+                height="h-40"
+                required
+                bg="bg-gray-100"
+                {...register('message')}
+              />
+            </div>
+
+            <div className="mt-4">
+              <button
+                type="submit"
+                disabled={submitting || submitted}
+                className="btn w-full px-2 py-2 rounded-sm bg-secondary hover:text-primary text-beige font-semibold uppercase  disabled:text-gray-300"
+              >
+                Envoyer !
+              </button>
+            </div>
+            {submitted && (
+              <div className="mb-4 sm:mb-6 bg-green-500 py-4 rounded-sm text-center">
+                <h3 className="text2xl text-white">Message successfully sent!</h3>
+              </div>
+            )}
+          </form>
         </div>
-
-        <form
-          onSubmit={handleSubmit}
-          noValidate
-          className="relative w-full p-4 sm:py-6 md:py-8 md:px-10"
-        >
-          {serverError && (
-            <div className="mb-4 sm:mb-6 bg-red-500 py-4 rounded-sm text-center">
-              <h3 className="text2xl text-white">{serverError}</h3>
-            </div>
-          )}
-          {submitted && (
-            <div className="mb-4 sm:mb-6 bg-green-500 py-4 rounded-sm text-center">
-              <h3 className="text2xl text-white">Message successfully sent!</h3>
-            </div>
-          )}
-          <div className="flex flex-col gap-2">
-
-            <Input
-              name="name"
-              label="Nom"
-              required
-              bg="bg-gray-100"
-              {...register('name')}
-            />
-            <Input
-              name="email"
-              label="Email"
-              required
-              bg="bg-gray-100"
-              {...register('email')}
-            />
-            <Input name="phone" label="Numéro de téléphone" bg="bg-gray-100" {...register('phone')} />
-            <TextArea
-              name="message"
-              label="Message"
-              height="h-40"
-              required
-              bg="bg-gray-100"
-              {...register('message')}
-            />
-          </div>
-
-          <div className="mt-4">
-            <button
-              type="submit"
-              disabled={submitting || submitted}
-              className="btn w-full px-2 py-2 rounded-sm bg-secondary hover:text-primary text-beige font-semibold uppercase  disabled:text-gray-300"
-            >
-              Envoyer !
-            </button>
-          </div>
-          {submitted && (
-            <div className="mb-4 sm:mb-6 bg-green-500 py-4 rounded-sm text-center">
-              <h3 className="text2xl text-white">Message successfully sent!</h3>
-            </div>
-          )}
-        </form>
       </div>
     </div>
   )
